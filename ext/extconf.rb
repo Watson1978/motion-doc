1d0523832198f3845d61af1d4666f73ed239d700
Watson <watson1978@gmail.com>
2013-05-12 08:29:03 +0900

    make directory if not exist

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,8 +1,11 @@
 # Command-Line Plugin Installer
 require 'fileutils'
 
+dir = File.expand_path("~/Library/RubyMotion/command/")
 src = File.expand_path(File.join(File.dirname(__FILE__), "../command/motion-doc.rb"))
-dst =  File.expand_path("~/Library/RubyMotion/command/motion-doc.rb")
+dst = File.join(dir, "motion-doc.rb")
+
+FileUtils.mkdir_p(dir) unless File.exist?(dir)
 FileUtils.rm dst if File.exist?(dst)
 FileUtils.ln_s src, dst
 
